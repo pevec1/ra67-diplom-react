9d52501548091a646321cecc97392b7fa1dd7eac
Andrei Harenkov <pevec1@yandex.ru>
2024-08-23 11:38:57 +0300

    update 23-08-2024 v0.8.2

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -4,19 +4,21 @@ import CatalogHome from "../components/CatalogHome";
 
 export default function Catalog() {
   return (
-        <main className="container">
+    <main className="container">
       <div className="row">
         <div className="col">
           <div className="banner">
-            <img src="./img/banner.jpg" className="img-fluid" alt="К весне готовы!"/>
+            <img
+              src="https://github.com/pevec1/ra67-diplom-react/blob/main/img/banner.jpg?raw=true"
+              className="img-fluid"
+              alt="К весне готовы!"
+            />
             <h2 className="banner-header">К весне готовы!</h2>
           </div>
 
-    <CatalogHome />
-
-            </div>
+          <CatalogHome />
+        </div>
       </div>
     </main>
-
-  )
+  );
 }
