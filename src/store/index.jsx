f9ae2105af0b66721310c4d060c44a006fdbf754
Andrei Harenkov <pevec1@yandex.ru>
2024-08-27 10:37:26 +0300

    update 27-08-2024 v0.8.3

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,11 +1,2 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { searchReducer } from './../slices/searchSlice';
-
 export * from "../slices/api";
 export * from "./store";
-export default configureStore({
-  reducer: {
-    search: searchReducer,
-  },
-});
-
