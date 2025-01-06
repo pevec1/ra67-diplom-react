052d3ab56765e664de4982da485bebfd1a4a9566
Andrei Harenkov <pevec1@yandex.ru>
2025-01-06 08:45:41 +0300

    update 2025-01-06

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import "./style.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
+  <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
   <Provider store={store}>
     <App />
   </Provider>
+  </BrowserRouter>
   //</React.StrictMode>,
 );
